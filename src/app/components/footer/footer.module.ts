7d5860c82055981919ef977362f9c5c8b7df493d
Bruno Papait <brunohpapait@gmail.com>
2024-09-18 09:22:39 -0300

    NgModule vs Standalone

---
+++ src/app/components/footer/footer.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./footer.component";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [],
+  exports: [FooterComponent]
+})
+
+export class FooterModule { }
